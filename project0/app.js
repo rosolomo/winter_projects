e1cd17999eceaa74e3bc20849183b054ad16d966
Robel Solomon <rosolomo@users.noreply.github.com>
2021-01-09 20:25:29 -0500

    converting js to react native

--- project0/app.js
+++ project0/app.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import { View, Button, Text, ScrollView, StyleSheet, Switch } from 'react-native'
-import { Constants } from 'expo'
+import {View, Button, Text, ScrollView, StyleSheet, Switch} from 'react-native'
+import {Constants} from 'expo'
 
 let id = 0
 
-const Todo = props => { <
-    View >
-        <
-        Button > < /Button> <
-        Text > { props.todo.text } < /Text> <
-        /View>
-}
+const Todo = props => (
+    <View>
+    <Switch value={props.todo.isChecked} onValueChange={props.onToggle} />
+    <Text> {props.todo.text} </Text>
+    <Button title="delete" onPress={props.onDelete} />
+    </View>
+)
 
 class App extends React.Component {
 
@@ -18,40 +18,55 @@ class App extends React.Component {
         todos: []
     }
 
-    addTodo() {
+    addTodo (){
         id++
         const text = `item # ${id}`
-        this.setState({
-            todo: [
+        this.setState ({
+            todos: [
                 ...this.state.todos,
-                { id = id, text: text, isChecked: false }
-            ]
+                {id: id, text: text, isChecked: false},
+            ],
         })
     }
 
-    removeTodo(id) {
+    removeTodo (id){
         this.setState({
-            todos: this.state.todos.filter(todo => todo.id != id)
+        todos: this.state.todos.filter(todo => todo.id != id)
+    })
+}
+
+toggleChecked (id){
+    this.setState({
+        todos: this.state.todos.map (todo => {
+            if (todo.id !== id) return todo
+            return {
+                id: todo.id,
+                text: todo.text,
+                isChecked: !todo.isChecked
+            }
+
         })
-    }
-
-    toggleChecked(id) {
-        this.setState({
-            todos: this.state.todo.map(todo => {
-                if (todo.id !== id) return todo
-                return {
-                    isChecked = !todo.isChecked
-                }
-
-            })
-        })
-    }
+    })
+}
 
 
-
-
-    render() {
-        return null
-    }
-
+render() {
+    return (
+      <View>
+        <Text>TODO LIST</Text>
+        <Text>Todo count: {this.state.todos.length}</Text>
+        <Text> Unchecked count: {this.state.todos.filter((todo) => !todo.isChecked).length} </Text>
+        <Button onPress={() => this.addTodo()} title="Add todo" />
+        <ScrollView>
+          {this.state.todos.map((todo) => (
+            <Todo
+              onToggle={() => this.toggleChecked(todo.id)}
+              onDelete={() => this.removeTodo(todo.id)}
+              todo={todo}
+            />
+          ))}
+        </ScrollView>
+      </View>
+    );
+  }
 }
